cbe8fa676212579547721abbeeeb0e659587aad0
Erik Hartley <erik>
2018-02-10 14:37:40 -0700

    Initial Commit

---
+++ SolarSystem-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SolarSystem-C
+//
+//  Created by Erik HARTLEY on 2/10/18.
+//  Copyright © 2018 Erik HARTLEY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SolarSystem-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SolarSystem-C
+//
+//  Created by Erik HARTLEY on 2/10/18.
+//  Copyright © 2018 Erik HARTLEY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
